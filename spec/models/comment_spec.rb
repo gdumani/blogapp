ba7c63146129a0f313530879216f19ed6835dfbc
gdumani <gdumani@gmail.com>
2023-06-10 11:52:31 -0600

    Write tests for users posts comments likes models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:testuser) { User.new(name: 'Testuser', photo: 'www.photo', bio: 'Test bio', posts_counter: 0) }
+  let(:testpost) { Post.new author: testuser, title: 'title', comments_counter: 0, likes_counter: 0 }
+  let(:subject) { Comment.new(user: testuser, post: testpost) }
+
+  before do
+    testuser.save
+    testpost.save
+    subject.save
+  end
+
+  it 'should increment comments_counter in posts' do
+    expect(testpost.comments_counter).to be(1)
+  end
 end

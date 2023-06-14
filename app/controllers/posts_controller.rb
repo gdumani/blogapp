5dfff22ca38e95ffb835ed9de097589632d01e58
gdumani <gdumani@gmail.com>
2023-06-14 04:16:03 -0600

    add methods index and show to users  and posts add prefixes to routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params(:id))
+    @posts = @user.posts
   end
 
   def show
+    @user = User.find(params(:user_id))
+    @post = @user.posts.find(params(:id))
+    @comments = @post.comments
   end
 end

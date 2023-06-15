073fcfa3f3ef2d93d11cb64e07a32b7bb05bcba6
gdumani <gdumani@gmail.com>
2023-06-15 14:52:20 -0600

    create controller tests verify pages rendered on index and show methods

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  describe 'GET /index and render place holder page' do
     it 'returns http success' do
-      get '/users/index'
+      get '/users'
       expect(response).to have_http_status(:success)
+      expect(response.body.include?('List of users')).to be true
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET /show and render place holder page' do
     it 'returns http success' do
-      get '/users/show'
+      @usr = User.create name: 'Testuser', photo: 'www.photo', bio: 'Test bio', posts_counter: 0
+      get "/users/#{@usr.id}"
       expect(response).to have_http_status(:success)
+      expect(response.body.include?('Given user with recent posts')).to be true
     end
   end
 end

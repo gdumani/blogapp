9a1e03319c09f40d2a95c031560b771ab5bbd29b
gdumani <gdumani@gmail.com>
2023-06-16 09:36:41 -0600

    fix tests to avoid unused placeholders

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   before do
-    @usr = User.create name: 'Testuser', photo: 'www.photo', bio: 'Test bio', posts_counter: 0
-    @pst = Post.create author: @usr, title: 'title', comments_counter: 0, likes_counter: 0
+    @usr = User.create name: 'Testuser', photo: 'nophoto.png', bio: 'Test bio', posts_counter: 0
+    @pst = Post.create author: @usr, title: 'title', text: '', comments_counter: 0, likes_counter: 0
   end
 
   describe 'GET /index and retrieve place holder page' do
@@ -10,7 +10,7 @@ RSpec.describe 'Posts', type: :request do
       get "/users/#{@usr.id}/posts"
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
-      expect(response.body.include?('List of posts for a given user')).to be true
+      # expect(response.body.include?('List of posts for a given user')).to be true
     end
   end
 
@@ -19,7 +19,7 @@ RSpec.describe 'Posts', type: :request do
       get "/users/#{@usr.id}/posts/#{@pst.id}"
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
-      expect(response.body.include?('Given post of given user')).to be true
+      # expect(response.body.include?('Given post of given user')).to be true
     end
   end
 end

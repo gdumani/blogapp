f869df652a88247e3698031e2192241fdc323685
gdumani <gdumani@gmail.com>
2023-06-17 17:43:07 -0600

    Add new  posts  form Add create and new controls and routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
+  get '/posts/new', to: 'posts#new', as: 'post_new'
+  post 'posts/new', to: 'posts#create', as: 'post_create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

9dfc21552d2c7aa8268f2e40df9510227bb9865f
gdumani <gdumani@gmail.com>
2023-06-20 19:19:50 -0600

    fix bugs add images eliminate unused testfiles

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:testuser) { User.new(name: 'Testuser', photo: 'www.photo', bio: 'Test bio', posts_counter: 0) }
   let(:testpost) { Post.new author: testuser, title: 'title', comments_counter: 0, likes_counter: 0 }
-  let(:subject) { Comment.new(user: testuser, post: testpost) }
+  let(:subject) { Comment.new(user: testuser, post: testpost, text: 'Comment text') }
 
   before do
     testuser.save
@@ -12,6 +12,6 @@ RSpec.describe Comment, type: :model do
   end
 
   it 'should increment comments_counter in posts' do
-    expect(testpost.comments_counter).to be(1)
+    expect(testpost.comments_counter).to eq(1)
   end
 end

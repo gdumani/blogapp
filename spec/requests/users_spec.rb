bb41448a2e44cbe1083649ea5627f8f31bdc011c
gdumani <gdumani@gmail.com>
2023-09-07 12:53:32 -0600

    Update users integration specs

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index and render place holder page' do
+    it 'returns http success' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      # expect(response.body.include?('List of users')).to be true
+    end
+  end
+
+  describe 'GET /show and render place holder page' do
+    it 'returns http success' do
+      @usr = User.create name: 'Testuser', photo: 'nophoto.png', bio: 'Test bio', posts_counter: 0
+      get "/users/#{@usr.id}"
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      # expect(response.body.include?('Given user with recent posts')).to be true
+    end
+  end
+end
